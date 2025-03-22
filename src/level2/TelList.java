1ca50b5137a5e24a8791cfbdb6bdf7887bc14a4d
EZUUNUU <ezuunuu@gmail.com>
2025-03-23 00:46:10 +0900

    1,2 레벨 추가

---
+++ src/level2/TelList.java
@@ -0,0 +1,21 @@
+
+
+public class TelList {
+    public static void main(String[] args) {
+        String[] ex1 = {"119", "97674223", "1195524421"};
+        boolean answer1 = false;
+        System.out.println(solution(ex1));
+        String[] ex2 = {"123","456","789"};
+        System.out.println(solution(ex2));
+        boolean answer2 = true;
+        String[] ex3 = {"12","123","1235","567","88"};
+        System.out.println(solution(ex3));
+        boolean answer3 = false;
+    }
+
+    static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        return answer;
+    }
+}
